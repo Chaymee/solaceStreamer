5eef050d7e8918dc324b07f195fbbb23f497fa87
Chaymee <fr.golgo@gmail.com>
2022-03-28 22:24:52 -0500

    Added command line args parser

--- producer.py
+++ producer.py
@@ -2,6 +2,7 @@
 
 from email.message import Message
 import time
+import sys
 import cv2
 from solace.messaging.config import _sol_constants
 from solace.messaging.messaging_service import MessagingService
@@ -17,6 +18,8 @@ broker_props = {
     "solace.messaging.authentication.scheme.basic.username": os.environ.get('SOLACE_USERNAME') or "default",
     "solace.messaging.authentication.scheme.basic.password": os.environ.get('SOLACE_PASSWORD') or "default"
     }
+TOPIC = ''
+INPUTFILE = ''
 
 def video_emitter(video):
     # Open the video file
@@ -40,6 +43,8 @@ def video_emitter(video):
         try:
             messagingService.connect()
             destination = Topic.of('solace/test/topic')
+        finally: 
+            print('didnt send')
         direct_message_publish(messagingService, destination, bytearray(jpeg))
         # Sleep to curtail CPU usage
         time.sleep(0.2)
@@ -58,6 +63,20 @@ def direct_message_publish(messaging_service: MessagingService, destination, mes
         direct_publish_service.publish(destination=destination,message=message)
     finally:
         direct_publish_service.terminate()
+
+
+def readParms():
+    if sys.argv.count != 7:
+        print('**USAGE** host vpnname userID password topic infile') 
+        exit()
+    print('Command line arguments: ' + str(sys.argv))
+    broker_props["solace.messaging.transport.host"] = str(sys.argv[1])
+    broker_props["solace.messaging.service.vpn-name"] = str(sys.argv[2])
+    broker_props["solace.messaging.authentication.scheme.basic.username"] = str(sys.argv[3])
+    broker_props["solace.messaging.authentication.scheme.basic.password"] = str(sys.argv[4])
+    TOIPC = str(sys.argv[5])
+    INPUTFILE = str(sys.argv[6])
+
 
 
 if __name__ == '__main__':
