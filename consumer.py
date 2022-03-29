7fbb2b77ef1bc25dd9553021b95e92d79144ed02
Jamieson Walker <Chaymee@users.noreply.github.com>
2022-03-29 14:57:26 -0500

    finished initial prototype

---
+++ consumer.py
@@ -0,0 +1,63 @@
+# consumer.py
+from flask import Flask, Response
+import sys
+
+from solace.messaging.messaging_service import MessagingService
+from solace.messaging.receiver.inbound_message import InboundMessage
+from solace.messaging.receiver.message_receiver import MessageHandler
+from solace.messaging.resources.topic import Topic
+from solace.messaging.resources.topic_subscription import TopicSubscription
+from solace.messaging.utils.converter import BytesToObject
+from solace.messaging.receiver.direct_message_receiver import DirectMessageReceiver
+
+# Broker Config. Note: Could pass other properties Look into
+broker_props = {
+    "solace.messaging.transport.host": '',
+    "solace.messaging.service.vpn-name": '', 
+    "solace.messaging.authentication.scheme.basic.username": '',
+    "solace.messaging.authentication.scheme.basic.password": ''
+    }
+
+
+
+# Continuously listen to the connection and print a recieved message
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # return a multipart response
+    return Response(solaceStream(), 
+                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def solaceStream():
+    
+    while True:
+        message_payload = receiver.receive_message().get_payload_as_bytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/png\r\n\r\n' + message_payload + b'\r\n\r\n')
+
+def readParms():
+    global broker_props
+    global TOPIC
+    global INPUTFILE
+    print('args: ' + str(sys.argv) + str(len(sys.argv)))
+    if len(sys.argv) != 6:
+        print('**USAGE** host vpnname userID password topic infile') 
+        exit()
+    print('Command line arguments: ' + str(sys.argv))
+    broker_props["solace.messaging.transport.host"] = str(sys.argv[1])
+    broker_props["solace.messaging.service.vpn-name"] = str(sys.argv[2])
+    broker_props["solace.messaging.authentication.scheme.basic.username"] = str(sys.argv[3])
+    broker_props["solace.messaging.authentication.scheme.basic.password"] = str(sys.argv[4])
+    TOPIC = str(sys.argv[5])
+
+                
+if __name__ == '__main__':
+    readParms()
+    # Connect to solace server and pass the topic we want to consume
+    service = MessagingService.builder().from_properties(broker_props).build()
+    service.connect()
+    topics = [TopicSubscription.of(TOPIC)]
+    receiver: DirectMessageReceiver = service.create_direct_message_receiver_builder().with_subscriptions(topics).build()
+    receiver.start()
+    app.run()
--- producer.py
+++ producer.py
@@ -7,47 +7,53 @@ import cv2
 from solace.messaging.config import _sol_constants
 from solace.messaging.messaging_service import MessagingService
 from solace.messaging.resources.topic import Topic
-from solace.messaging.utils.converter import ObjectToBytes
 from solace.messaging.utils.manageable import Metric
 
 
 # Broker Config. Note: Could pass other properties Look into
 broker_props = {
-    "solace.messaging.transport.host": os.environ.get('SOLACE_HOST') or "tcp://localhost:55555,tcp://localhost:55554",
-    "solace.messaging.service.vpn-name": os.environ.get('SOLACE_VPN') or "default",
-    "solace.messaging.authentication.scheme.basic.username": os.environ.get('SOLACE_USERNAME') or "default",
-    "solace.messaging.authentication.scheme.basic.password": os.environ.get('SOLACE_PASSWORD') or "default"
+    "solace.messaging.transport.host": '',
+    "solace.messaging.service.vpn-name": '', 
+    "solace.messaging.authentication.scheme.basic.username": '',
+    "solace.messaging.authentication.scheme.basic.password": ''
     }
 TOPIC = ''
 INPUTFILE = ''
 
-def video_emitter(video):
+def video_emitter():
     # Open the video file
-    video = cv2.VideoCapture(video)
+    print('Reading video file: ' + INPUTFILE)
+    try:
+        video = cv2.VideoCapture(INPUTFILE)
+    except:
+        exit(0)
+
     print(' emitting.....')
-    messageService = MessagingService.builder().from_properties(broker_props)\
-                        .build()
+    messageService = MessagingService.builder().from_properties(broker_props).build()
     
     messageService.connect()
+    destination_name = Topic.of(TOPIC)
     
     # read the file
     while (video.isOpened):
         # read the image in each frame
         success, image = video.read()
+        #downscale video
+        scale_percent = 40
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        output = cv2.resize(image, dim, interpolation= cv2.INTER_AREA)
         # Check if the file has been read to the end
         if not success:
             break
         # Convert image to png
-        ret, jpeg = cv2.imencode('.png', image)
+        ret, jpeg = cv2.imencode('.png', output)
         # Convert image to bytes and send over kafka
-        try:
-            messagingService.connect()
-            destination = Topic.of('solace/test/topic')
-        finally: 
-            print('didnt send')
-        direct_message_publish(messagingService, destination, bytearray(jpeg))
+
+        direct_message_publish(messageService, destination_name, bytearray(jpeg))
         # Sleep to curtail CPU usage
-        time.sleep(0.2)
+        #time.sleep(0.2)
     # Clear the capture
     video.release()
     print('done emitting')
@@ -56,8 +62,7 @@ def video_emitter(video):
 def direct_message_publish(messaging_service: MessagingService, destination, message):
 
     try:
-        direct_publish_service = messaging_service.create_direct_message_publisher_builder().\
-            on_back_pressure_reject(buffer_capacity=0).build()
+        direct_publish_service = messaging_service.create_direct_message_publisher_builder().build()
         pub_start = direct_publish_service.start_async()
         pub_start.result()
         direct_publish_service.publish(destination=destination,message=message)
@@ -66,7 +71,11 @@ def direct_message_publish(messaging_service: MessagingService, destination, mes
 
 
 def readParms():
-    if sys.argv.count != 7:
+    global broker_props
+    global TOPIC
+    global INPUTFILE
+    print('args: ' + str(sys.argv) + str(len(sys.argv)))
+    if len(sys.argv) != 7:
         print('**USAGE** host vpnname userID password topic infile') 
         exit()
     print('Command line arguments: ' + str(sys.argv))
@@ -74,10 +83,11 @@ def readParms():
     broker_props["solace.messaging.service.vpn-name"] = str(sys.argv[2])
     broker_props["solace.messaging.authentication.scheme.basic.username"] = str(sys.argv[3])
     broker_props["solace.messaging.authentication.scheme.basic.password"] = str(sys.argv[4])
-    TOIPC = str(sys.argv[5])
+    TOPIC = str(sys.argv[5])
     INPUTFILE = str(sys.argv[6])
 
 
 
 if __name__ == '__main__':
-    video_emitter('video.mp4')
+    readParms()
+    video_emitter()
